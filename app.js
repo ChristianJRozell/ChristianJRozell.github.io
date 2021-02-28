bd4dfa698696ef00f9b2c08d5dc34053ec71f0f2
Christian Rozell <ChristianJRozell@gmail.com>
2021-02-27 16:11:25 -0800

    hopefully fixed config

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const sessions = require('client-sessions');
 
 // const connection_string = require('./secrets/not_my_connection_string.txt');
 const mongoose = require('mongoose');
-const mongoDB = process.env.connection_string;
-console.log('yeet', process.env.connection_string);
+const mongoDB = process.env.CONNECTION_STRING;
+console.log('yeet', process.env.CONNECTION_STRING);
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
